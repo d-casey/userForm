701538f1946e2ae61a18f55988509d97978e91ef
Damian Casey <damian.casey@HX3347T2.local>
2020-06-10 12:45:28 +0100

    a bit neater

--- src/containers/Main/Main.test.js
+++ src/containers/Main/Main.test.js
@@ -4,39 +4,42 @@ import { Provider } from 'react-redux'
 
 import renderer from 'react-test-renderer'
 import configureStore from 'redux-mock-store'
-
 const mockStore = configureStore([])
 
-describe('<Main /> container', () => {
-  let store
-  let component
-  let initialState = {
-    app: {
-      stage: 0,
-      formCompleted: true
-    },
-    newUserForm: {
-      name: "",
-      role: "",
-      email: "",
-      password: "",
-      isSubmitted: false
-    },
-    privacyForm : {
-      isSubmitted: false,
-      receive_updates: true,
-      receive_communication: false,
-    }
+let store
+let component
+let initialState = {
+  app: {
+    stage: 0,
+    formCompleted: true
+  },
+  newUserForm: {
+    name: "",
+    role: "",
+    email: "",
+    password: "",
+    isSubmitted: false
+  },
+  privacyForm : {
+    isSubmitted: false,
+    receive_updates: true,
+    receive_communication: false,
   }
+}
 
+const getWrapper = (state) => {
+  store = mockStore(state)
+  component = renderer.create(
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  )
+}
+
+describe('<Main /> container', () => {
   describe('When the app stage is 0', () => {
     beforeEach(() => {
-      store = mockStore(initialState)
-      component = renderer.create(
-        <Provider store={store}>
-          <Main />
-        </Provider>
-      )
+      getWrapper(initialState)
     })
 
     it('passes the state into the sidebar and progress bar', () => {
@@ -46,30 +49,21 @@ describe('<Main /> container', () => {
 
   describe('When the app stage is 1', () => {
     beforeEach(() => {
-      initialState.app.stage = 1
-      store = mockStore(initialState)
-      component = renderer.create(
-        <Provider store={store}>
-          <Main />
-        </Provider>
-      )
+      let updatedState = initialState
+      updatedState.app.stage = 1
+      getWrapper(updatedState)
     })
 
     it('should render the correct sidebar and progress bar content', () => {
       expect(component.toJSON()).toMatchSnapshot()
     })
-
   })
 
   describe('When the app stage is 2', () => {
     beforeEach(() => {
-      initialState.app.stage = 2
-      store = mockStore(initialState)
-      component = renderer.create(
-        <Provider store={store}>
-          <Main />
-        </Provider>
-      )
+      let updatedState = initialState
+      updatedState.app.stage = 2
+      getWrapper(updatedState)
     })
 
     it('should render the correct sidebar and progress bar content', () => {
